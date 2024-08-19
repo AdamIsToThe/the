4ab67c8abc8d0b2774f1deec89c9c6d4a32adfad
AdamIsToThe <adamabbadi48@gmail.com>
2024-08-19 19:20:57 +0100

    Update save.py

--- save.py
+++ save.py
@@ -1,1 +1,23 @@
 
+from discord import Client
+
+
+sp = Client()
+
+
+
+@sp.event
+async def on_ready():
+
+    with open('msgs.txt', 'a+') as fp:
+        async for msg in (await sp.fetch_channel(1261436996044456037)).history(
+            limit=9000000, oldest_first=True):
+            print(msg.id)
+            fp.write(msg.content + '\n')
+            fp.write('\n'.join([x.proxy_url for x in msg.attachments]))
+
+    
+
+
+
+sp.run()
